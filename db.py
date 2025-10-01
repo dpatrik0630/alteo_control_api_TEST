a87d9c836406f07144af217f55e876bd9657aea1
d.patrik0630 <d.patrik0630@gmail.com>
2025-10-01 09:54:34 +0200

    Creating .env for secret variables, .env.example for empty secret variables, .gitignore and Dockerfile

--- db.py
+++ db.py
@@ -1,11 +1,12 @@
+import os
 import psycopg2
 
 def get_db_connection():
     conn = psycopg2.connect(
-        dbname="aramut",
-        user="postgres",
-        password="mj46-pr23",
-        host="100.115.164.70",
-        port="5432"
+        dbname=os.environ.get("DB_NAME"),
+        user=os.environ.getenv("DB_USER"),
+        password=os.environ.getenv("DB_PASSWORD"),
+        host=os.environ.getenv("DB_HOST"),
+        port=os.environ.getenv("DB_PORT")
     )
     return conn
