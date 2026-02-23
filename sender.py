f83fb2debd06ea5ddf6ebfadc3a1e21b4d43d174
d.patrik0630 <d.patrik0630@gmail.com>
2026-02-23 11:58:04 +0100

    Modfying apu url to prod

--- sender.py
+++ sender.py
@@ -11,7 +11,7 @@ from psycopg2.extras import RealDictCursor
 
 USE_HEARTBEAT = False
 
-API_URL = "https://apim-ap-test.azure-api.net/plant-control/api/setpoint"
+API_URL = "https://ams-partner-api.azure-api.net/plant-control/api/setpoint"
 
 CYCLE_TIME = 2
 MAX_WORKERS = 8
